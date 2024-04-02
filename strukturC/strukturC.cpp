8ca849950add8e48fd590ccbabe0409cf1e489d4
sabilafiridas <sabilafirdias@gmail.com>
2024-04-02 10:51:11 +0700

    Add project files.

---
+++ strukturC/strukturC.cpp
@@ -0,0 +1,17 @@
+// strukturC.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace;
+
+struct Mahasiswa {
+    string nim;
+    string nama;
+    string alamat;
+    string umur;
+};
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
