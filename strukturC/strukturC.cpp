f7669dc6aae5a5a288a0845eb0eefb73ef23b5e1
sabilafiridas <sabilafirdias@gmail.com>
2024-04-02 11:37:53 +0700

    membuat struct detail alamat

--- strukturC/strukturC.cpp
+++ strukturC/strukturC.cpp
@@ -8,7 +8,7 @@ struct Mahasiswa {
     string nim;
     string nama;
     string alamat;
-    string umur;
+    int umur;
 };
 
 int main()
---
+++ strukturC_2/strukturC_2.cpp
@@ -0,0 +1,16 @@
+// strukturC_2.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+struct DetailAlamat {
+    string desa;
+    string kota;
+};
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
