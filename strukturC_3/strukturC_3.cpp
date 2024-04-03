6253193f6c37a457a0ff2d179e715033cb21eb04
sabilafiridas <sabilafirdias@gmail.com>
2024-04-03 11:53:10 +0700

    menambahkan getline pada int main nim dan nama mahasiswa

---
+++ strukturC_3/strukturC_3.cpp
@@ -0,0 +1,43 @@
+// strukturC_3.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+struct DetailAlamat {
+    string desa;
+    string kota;
+};
+
+struct Mahasiswa {
+    string nim;
+    string nama;
+    DetailAlamat alamat;
+    int umur;
+};
+
+int main()
+{
+    Mahasiswa mhs;
+    cout << "Nomor Mahasiswa : ";
+    getline(cin,mhs.nim);
+    cout << "Nama Mahasiswa : ";
+    getline(cin,mhs.nama);
+
+    cout << "Alamat Mahasiswa : " << endl;
+    cout << "\t Nama Desa : ";
+    cin >> mhs.alamat.desa;
+    cout << "\t Nama Kota : ";
+    cin >> mhs.alamat.kota;
+
+    cout << "Umur Mahasiswa : ";
+    cin >> mhs.umur;
+
+    cout << endl;
+    cout << "\n NIM : " << mhs.nim;
+    cout << "\n Nama : " << mhs.nama;
+    cout << "\n Alamat : ";
+    cout << "\n \t Desa : " << mhs.alamat.desa;
+    cout << "\n \t Kota : " << mhs.alamat.kota;
+    cout << "\n Umur : " << mhs.umur;
+}
