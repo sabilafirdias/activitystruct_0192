ec8e425856059c67080ab3af9073572877309380
sabilafiridas <sabilafirdias@gmail.com>
2024-04-03 11:26:08 +0700

    membuat int main alamat mahasiswa

--- strukturC_2/strukturC_2.cpp
+++ strukturC_2/strukturC_2.cpp
@@ -23,5 +23,11 @@ int main()
     cin >> mhs.nim;
     cout << "Nama Mahasiswa : ";
     cin >> mhs.nama;
+
+    cout << "Alamat Mahasiswa : " << endl;
+    cout << "\t Nama Desa : ";
+    cin >> mhs.alamat.desa;
+    cout << "\t Nama Kota : ";
+    cin >> mhs.alamat.kota;
 }
 
